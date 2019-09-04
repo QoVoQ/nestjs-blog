960cf6281d65f49dceba1fc90303b3de06091bf1
qovoq <674263588@qq.com>
2019-09-04 15:03:46 +0800

    🎉 Boilplate

---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,29 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('AppController (e2e)', () => {
+  let app;
+  let server;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    server = app.getHttpServer();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    app.close();
+  });
+
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!');
+  });
+});
